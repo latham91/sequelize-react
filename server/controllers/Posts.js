87c18bc948f2f35bf30acdac903419e21479061e
Aaron Latham <latham91@icloud.com>
2024-01-13 14:29:58 +0000

    fixed typo

--- server/controllers/Posts.js
+++ server/controllers/Posts.js
@@ -4,11 +4,11 @@ const getAllPosts = async (req, res) => {
     try {
         const allPosts = await posts.findAll();
 
-        if (posts.length === 0) {
-            return res.status(404).json({ success: false, count: posts.length, message: "No posts found" });
+        if (allPosts.length === 0) {
+            return res.status(404).json({ success: false, count: allPosts.length, message: "No posts found" });
         }
 
-        return res.status(200).json({ success: true, count: posts.length, posts: allPosts });
+        return res.status(200).json({ success: true, count: allPosts.length, posts: allPosts });
     } catch (error) {
         return res.status(500).json({ success: false, message: "Unable to retrieve posts", error: error.message });
     }
--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ require("colors");
 
 const app = express();
 app.use(express.json());
-app.use(morgan("dev"));
+
+// Development logging
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
 
 // Route imports
 const postsRouter = require("./routes/Posts");
@@ -15,7 +19,7 @@ const postsRouter = require("./routes/Posts");
 // Route middleware
 app.use("/api/posts", postsRouter);
 
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync().then(() => {
     app.listen(process.env.PORT, () => {
         console.log(`Server is running on http://localhost:${process.env.PORT}`.bgWhite.black);
     });
